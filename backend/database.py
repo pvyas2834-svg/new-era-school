eebcc8d1371bb7cbe588f54b6b0cb47d5c19357b
Prachi <pvyas2834@gmail.com>
2026-08-19 23:59:48 +0530

    Fix production backend URL

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,14 @@
+import os
 import mysql.connector
 
 
 def get_db():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Prachi@2106",
-        database="new_era_school"
+        host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", "3306")),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", ""),
+        database=os.getenv("DB_NAME", "new_era_school")
     )
 
 
